21ca625239267ff8cfadc8f87702b4063c28cd44
maesiltea <maesil007@gmail.com>
2025-11-22 17:43:28 +0900

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InstagramMain extends StatefulWidget {
+  const InstagramMain({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<InstagramMain> createState() => _InstagramMainState();
+}
+
+class _InstagramMainState extends State<InstagramMain> {
+  Widget _buildBottomTab(VoidCallback onTap, IconData icon) => Expanded(
+    child: InkWell(
+      onTap: onTap,
+      child: Container(
+        color: Colors.transparent,
+        child: Center(
+          child: Icon(icon),
+        ),
+      ),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black
+        ),
+        leading: IconButton(icon: Icon(Icons.camera_alt), onPressed: () {},),
+        elevation: 1,
+        backgroundColor: Theme.of(context).canvasColor,
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.send), onPressed: () {},),
+        ]
+      ),
+      body: Center(
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            _buildBottomTab((){}, Icons.home),
+            _buildBottomTab((){}, Icons.search),
+            _buildBottomTab((){}, Icons.add_box),
+            _buildBottomTab((){}, Icons.favorite),
+            _buildBottomTab((){}, Icons.account_circle),
+          ]
+        )
+      )
+    );
+  }
+}
