dfbf65aa05018f50ba5dc86cc58f80d6b3222d88
maesiltea <maesil007@gmail.com>
2025-11-26 16:28:59 +0900

    feat: 이미지 게시물 두 번 터치하면 좋아요 하는 기능 추가

--- lib/widgets/post_cards/image_post_card.dart
+++ lib/widgets/post_cards/image_post_card.dart
@@ -48,7 +48,6 @@ class _ImagePostCardState extends State<ImagePostCard> {
       children: [
         _buildHeader(post),
         _buildMedia(post),
-        // 여기서 공통 Footer 사용
         PostFooter(
           post: post,
           isLiked: _isLiked,
@@ -84,53 +83,62 @@ class _ImagePostCardState extends State<ImagePostCard> {
   }
 
   Widget _buildMedia(Post post) {
+    // 사진 1장
     if (post.mediaPaths.length == 1) {
-      return AspectRatio(
-        aspectRatio: 1,
-        child: Image.asset(
-          post.mediaPaths.first,
-          fit: BoxFit.cover,
+      return GestureDetector(
+        onDoubleTap: _toggleLike,
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: Image.asset(
+            post.mediaPaths.first,
+            fit: BoxFit.cover,
+          ),
         ),
       );
     }
 
-    return AspectRatio(
-      aspectRatio: 1,
-      child: Stack(
-        children: [
-          PageView.builder(
-            controller: _pageController,
-            itemCount: post.mediaPaths.length,
-            onPageChanged: (idx) {
-              setState(() => _currentPage = idx);
-            },
-            itemBuilder: (context, index) {
-              return Image.asset(
-                post.mediaPaths[index],
-                fit: BoxFit.cover,
-              );
-            },
-          ),
-          Positioned(
-            right: 12,
-            top: 12,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Text(
-                '${_currentPage + 1}/${post.mediaPaths.length}',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
+    // 사진 여러 장
+    return GestureDetector(
+      onDoubleTap: _toggleLike,
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Stack(
+          children: [
+            PageView.builder(
+              controller: _pageController,
+              itemCount: post.mediaPaths.length,
+              onPageChanged: (idx) {
+                setState(() => _currentPage = idx);
+              },
+              itemBuilder: (context, index) {
+                return Image.asset(
+                  post.mediaPaths[index],
+                  fit: BoxFit.cover,
+                );
+              },
+            ),
+            Positioned(
+              right: 12,
+              top: 12,
+              child: Container(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                decoration: BoxDecoration(
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Text(
+                  '${_currentPage + 1}/${post.mediaPaths.length}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
